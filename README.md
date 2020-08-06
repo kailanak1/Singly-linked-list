1353c5b40dc760e2cdf5c39267135dd6050c1ee5
kailana <kailanakahawaii@gmail.com>
2020-08-06 12:39:49 -0700

    add readme; shift; unshift; get; set;

---
+++ README.md
@@ -0,0 +1,18 @@
+# Singly Linked List in Javascript 
+
+
+Implementation for a Singly Linked List in Javascript. 
+
+Methods: 
+
+- Push 
+- Pop 
+- Index At 
+- Shift 
+- Unshift 
+- Get 
+- Set 
+- Insert 
+- Remove
+- Print 
+- Reverse 
--- singly-linked-list.js
+++ singly-linked-list.js
@@ -52,6 +52,113 @@ class SinglyLinkedList{
 		}
 		return node
 	}
+
+	shift(){
+		if(!this.head) return undefined
+		var current = this.head 
+		this.head = current.next 
+		this.length -- 
+		if(length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+		return current
+	}
+
+	unshift(val){
+		var newNode = new Node(val);
+		if(!this.head){
+			this.head = newNode 
+			this.tail = this.head  
+		} else {
+			newNode.next = this.head;  
+			this.head = newNode;
+		}
+		this.length++
+		return this
+	}
+
+	get(index){
+		if(index < 0 || index >= this.length) return null 
+
+		let counter = 0; 
+		var current = this.head;
+		while(counter !== index){
+			current = current.next 
+			counter++
+		}
+		return current
+	}
+
+	set(val, index){
+		let node = this.get(index);
+		if(node){
+			node.val = val; 
+			return true;
+		}
+		return false;
+	}
+
+	insert(val, index){
+		if(index < 0 || index > this.length) return false 
+		if(index === this.length){
+			//boolean !! operator negates return value, returns true/false
+			return !!this.push(val)
+		}
+		if(index === 0){
+			return !!this.unshift(val)
+		} else {
+			let newNode = new Node(val)
+			let previous = this.get(index -1)
+			//hold previous indexed node in a temp variable so as not to sever the connection
+			let temp = previous.next 
+			previous.next = newNode 
+			newNode.next = temp; 
+			this.length++ 
+			return true
+		}
+	}
 	
+	remove(index){
+		if(index < 0 || index >= this.length) return false 
+		if(index === this.length - 1) return !!this.pop()
+		if(index === 0) return !!this.shift()
+
+		let previous = this.get(index - 1)
+		let removed = previous.next 
+		previous.next = removed.next
+		this.length--
+		return removed
+	}
+
+	print(){
+		var arr = []
+		var current = this.head 
+		while(current){
+			arr.push(current.val)
+			current = current.next 
+		}
+		console.log(arr)
+	}
+
+	reverse(){
+		if(!this.head) return undefined
+		var node = this.head 
+		this.head = this.tail 
+		this.tail = node;
+		//need to make sure tail.next === null; so set prev to null
+		prev = null; 
+		var next; 
+
+		for( var i = 0; i < this.length; i++){
+			next = node.next 
+			node.next = prev; 
+			prev = node 
+			node = next;
+
+		}
+		return this
+
+	}
 	
 }
